6ab7cb36147844d8186b35273be15db002fdf990
gdgunnars <gdg@gdg.is>
2018-04-09 22:36:16 +0200

    Created more routes and redirects while developing
    This will be restructured a bit, but general path idea is set.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [
-  { 
-    path: '', 
+const routes: Routes = [{
+    path: '', redirectTo: 'athlete', pathMatch: 'full'
+  },{ 
+    path: 'athlete', 
     loadChildren: 'app/athletes/athletes.module#AthletesModule'
   },
   //{ path: 'login', component: Login },
--- src/app/athletes/athletes-routing.module.ts
+++ src/app/athletes/athletes-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AthleteListComponent } from './athlete-list/athlete-list.component';
+import { AthleteDetailComponent } from './athlete-detail/athlete-detail.component';
 
-const routes: Routes = [
-  {
-    path: '',
+const routes: Routes = [ {
+    path: '', redirectTo: 'list', pathMatch: 'full'
+  },{
+    path: 'list',
     component: AthleteListComponent
+  }, {
+    path: 'detail',
+    component: AthleteDetailComponent
   }
 ];
 
