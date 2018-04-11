f2ff1c7601d45389e8a886393048901daa9ea4a7
gdgunnars <gdg@gdg.is>
2018-04-11 23:57:37 +0200

    Added base functionality to details component.
    Created and placed two cards side by side. One to display athlete
    details (name, ssn, club) and another to show achievements.
    
    Set athletes-list as a temporary list to get an idea of how it will look
    with bouts list.

--- src/app/athletes/athlete-detail/athlete-detail.component.ts
+++ src/app/athletes/athlete-detail/athlete-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Athlete, AthletesService } from '../../service/athletes.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-athlete-detail',
@@ -8,9 +9,29 @@ import { Athlete, AthletesService } from '../../service/athletes.service';
 })
 export class AthleteDetailComponent implements OnInit {
 
+  fullName: String;
+  ssn: String;
+  club: String;
+
+  nameFormControl = new FormControl('', [ Validators.required]);
+  ssnFormControl = new FormControl('', [Validators.required]);
+  clubFormControl = new FormControl('', [Validators.required]);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getNameError() {
+    return this.nameFormControl.hasError('required') ? 'You must enter a name ' : 'Error in errors :O';
+  }
+
+  getSsnError() {
+    return this.ssnFormControl.hasError('required') ? 'You must enter a Social Security Number ' : 'Error in errors :O';
+  }
+
+  getClubError() {
+    return this.clubFormControl.hasError('required') ? 'You must enter a club ' : 'Error in errors :O';
   }
 
 }
