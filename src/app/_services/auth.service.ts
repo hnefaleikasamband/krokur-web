a966ca0e86624eb9c14fcff433560e2c12c6c373
gdgunnars <gdg@gdg.is>
2018-08-31 16:39:27 +0200

    code style change and log removing

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,28 +4,29 @@ import { map } from 'rxjs/operators';
 import AuthedUser from '../_models/authedUser';
 import { environment } from '../../environments/environment';
 
-
-
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private AuthenticatedUser: AuthedUser;
   public isAuthenticated = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
-    return this.http.post<any>(`${environment.baseUrl}/auth/login`, { email, password })
-      .pipe(map(user => {
-        // login successful if there's a jwt token in the response
-        if (user && user.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.setUserInfo(user);
-          this.isAuthenticated = true;
-        }
+    return this.http
+      .post<any>(`${environment.baseUrl}/auth/login`, { email, password })
+      .pipe(
+        map(user => {
+          // login successful if there's a jwt token in the response
+          if (user && user.token) {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.setUserInfo(user.user);
+            this.isAuthenticated = true;
+          }
 
-        return this.AuthenticatedUser;
-    }));
+          return this.AuthenticatedUser;
+        })
+      );
   }
 
   logout() {
@@ -40,9 +41,11 @@ export class AuthService {
   }
 
   getUserInfoFromJWT() {
-    return this.http.get<any>(`${environment.baseUrl}/auth/user`)
-      .pipe(map(user => {
-        if (!user) return user;
+    return this.http.get<any>(`${environment.baseUrl}/auth/user`).pipe(
+      map(user => {
+        if (!user) {
+          return user;
+        }
 
         if (user.token) {
           // store user details and jwt token in local storage to keep user logged in between page refreshes
@@ -53,15 +56,17 @@ export class AuthService {
         this.isAuthenticated = true;
 
         return this.AuthenticatedUser;
-      }));
+      })
+    );
   }
 
   private setUserInfo(user: any) {
+    console.log('setUserInfo fn, we got user:', user);
     this.AuthenticatedUser = {
       email: user.email,
       name: user.name,
       token: user.token,
       roles: ['']
-    }
+    };
   }
 }
