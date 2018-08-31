e8ec00a2564030bce1ef74dd1a00a2fe0aa36a20
gdgunnars <gdg@gdg.is>
2018-09-01 00:30:02 +0200

    error interc. only handles 401 now

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -7,20 +12,26 @@ import { AuthService } from '../_services/auth.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthService) {}
+  constructor(private authenticationService: AuthService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                const login = "/login";
-                if ( location.pathname.slice(0, login.length) !== login) {
-                    location.reload(true);
-                }
-            }
-            const error = err.error.message || err.statusText;
-            return throwError(error);
-        }))
-    }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(err => {
+        if (err.status === 401) {
+          // auto logout if 401 response returned from api
+          this.authenticationService.logout();
+          const login = '/login';
+          if (location.pathname.slice(0, login.length) !== login) {
+            location.reload(true);
+          }
+        }
+        const error = err.error.message || err.statusText;
+        //return throwError(error);
+        return throwError(err);
+      })
+    );
+  }
 }
