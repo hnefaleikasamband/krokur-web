88e9c56a267570dd18cdb56182221f5cfc6272a2
gdgunnars <gdg@gdg.is>
2018-09-02 00:21:50 +0200

    updated snackbar notifications & code style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from'@angular/forms';
-import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material'
+import { MatSnackBar } from '@angular/material';
 
-import { AuthService } from "../_services/auth.service";
+import { AuthService } from '../_services/auth.service';
 import { debuglog } from 'util';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  public defaultEmail: string = "admin@admin.com";
-  public defaultPassword: string = "1234";
-  
+  public defaultEmail = 'admin@admin.com';
+  public defaultPassword = '1234';
+
   loginForm: FormGroup;
   loading = false;
   returnUrl: string;
@@ -27,19 +26,13 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private authService: AuthService,
     private snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      password: ['', [
-        Validators.required,
-        Validators.minLength(5)
-      ]]
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
 
     // If user is logged in and manually goes to /login we will log the user
     // out and make them login again in case a new user is trying to log in.
@@ -49,16 +42,16 @@ export class LoginComponent implements OnInit {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.loginForm.value);
-
 
     if (this.loginForm.invalid) {
       return;
     }
 
     this.loading = true;
-    this.authService.login(this.loginForm.value.email, this.loginForm.value.password)
+    this.authService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
       .pipe(first())
       .subscribe(
         data => {
@@ -66,13 +59,20 @@ export class LoginComponent implements OnInit {
         },
         error => {
           this.loading = false;
-          if (error === "Unauthorized") {
-            this.snackBar.open('email or password not correct, please try again.','X', { duration: 4000});  
+          if (error.status === 401) {
+            this.snackBar.open(
+              'email or password not correct, please try again.',
+              'X',
+              { duration: 4000 }
+            );
           } else {
-            this.snackBar.open('Unkown error occured while trying to login.','X', { duration: 4000});
+            this.snackBar.open(
+              'Unkown error occured while trying to login.',
+              'X',
+              { duration: 4000 }
+            );
           }
-        });
-
+        }
+      );
   }
-  
 }
