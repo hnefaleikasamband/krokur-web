0769dcd63ca4610c0d3c9b91dd636c76f715605e
gdgunnars <gdg@gdg.is>
2018-09-16 14:30:55 +0200

    remove console log

--- src/app/athletes/athlete-bouts/athlete-bouts.component.ts
+++ src/app/athletes/athlete-bouts/athlete-bouts.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { AthletesService } from '../../_services/athletes.service';
 import { MatTableDataSource } from '@angular/material';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import Bout from '../../_models/bout';
 import Athlete from '../../_models/athlete';
-
 
 @Component({
   selector: 'app-athlete-bouts',
@@ -13,23 +17,29 @@ import Athlete from '../../_models/athlete';
   styleUrls: ['./athlete-bouts.component.css']
 })
 export class AthleteBoutsComponent implements OnInit {
-
   // Bout history
   dataSource = new MatTableDataSource();
-  columnsToDisplay = ['date','type','opponent','club','points','eventOrganizer'];
+  columnsToDisplay = [
+    'date',
+    'type',
+    'opponent',
+    'club',
+    'points',
+    'eventOrganizer'
+  ];
 
-  @Input() bouts: Bout[];
-  @Input() athlete: Athlete;
+  @Input()
+  bouts: Bout[];
+  @Input()
+  athlete: Athlete;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.dataSource.data = this.bouts;
   }
 
   newBout(bout: Bout) {
-    console.log('We got an event: ', bout);
     this.dataSource.data = [...this.dataSource.data, bout];
   }
-
 }
