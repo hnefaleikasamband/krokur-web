79e04b8e190331e030a80acf0e29f3de96231d0c
GDGunnars <gdg@trustpilot.com>
2019-04-09 00:05:29 +0200

    refactor manage-athlete & add structure

--- src/scripts/routes/manageAthletes/manageAthletes.js
+++ src/scripts/routes/manageAthletes/manageAthletes.js
@@ -1,85 +1,29 @@
 import React from "react";
-import MUIDataTable from "mui-datatables";
-import { StarHighlighter } from "../../components";
-import copy from "copy-to-clipboard";
+import Summary from "./summary";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { AthleteForm } from "../../components";
 
-const starHighlighterHelper = tableMeta => {
-  const dates = tableMeta.rowData ? tableMeta.rowData.slice(-8, -4) : [];
-  const [diploma, bronz, silver, gold] = dates;
-  const boutsLeft = tableMeta.rowData ? tableMeta.rowData.slice(-4) : [];
-  const [diplomaLeft, bronzLeft, silverLeft, goldLeft] = boutsLeft;
-  return (
-    <StarHighlighter
-      achievements={{ diploma, bronz, silver, gold }}
-      boutsLeftToAchievement={{ diplomaLeft, bronzLeft, silverLeft, goldLeft }}
-    />
-  );
-};
-
-const ManagedAthletes = ({ isFetching, athletes, club }) => {
-  const hideColumnOptions = { sort: false, filter: false, display: false };
-  const columns = [
-    { name: "id", options: hideColumnOptions },
-    { name: "name", label: "Name", options: { filter: false, sort: true } },
-    {
-      name: "ssn",
-      label: "Kennitala",
-      options: { filter: false, sort: false }
-    },
-    {
-      name: "achievements",
-      label: "Achievements",
-      options: {
-        hint: "Diploma / Bronz / Silver / Gold",
-        filter: false,
-        sort: false,
-        empty: true,
-        customBodyRender: (value, tableMeta) => starHighlighterHelper(tableMeta)
-      }
-    },
-    { name: "diplomaDate", options: hideColumnOptions },
-    { name: "bronzDate", options: hideColumnOptions },
-    { name: "silverDate", options: hideColumnOptions },
-    { name: "goldDate", options: hideColumnOptions },
-    { name: "diplomaBoutsLeft", options: hideColumnOptions },
-    { name: "bronzBoutsLeft", options: hideColumnOptions },
-    { name: "silverBoutsLeft", options: hideColumnOptions },
-    { name: "goldBoutsLeft", options: hideColumnOptions }
-  ];
-
-  const tableOptions = {
-    selectableRows: false,
-    filterType: "checkbox",
-    rowsPerPage: 20,
-    rowsPerPageOptions: [10, 20, 40],
-    downloadOptions: {
-      filename: "athletes.csv"
-    },
-    responsive: "stacked",
-    print: false,
-    viewColumns: false,
-    onRowClick: (rowData, { dataIndex, rowIndex }) => {
-      console.log("rowData:", rowData);
-      const data = rowData.reduce((acc, curr) => {
-        acc += `\t${curr}`;
-        return acc;
-      }, "");
-      copy(data.slice(data.indexOf(1)));
-      console.log("dataIndex:", dataIndex, "- rowIndex:", rowIndex);
-    }
-  };
+const ManageAthletes = ({ isFetching, athletes, club }) => {
   return (
     <div>
-      {!isFetching && (
-        <MUIDataTable
-          title={`Manage all athletes`}
-          data={athletes}
-          columns={columns}
-          options={tableOptions}
-        />
-      )}
+      <Grid container spacing={16} direction="row-reverse">
+        <Grid item xs={12} md={4}>
+          <Paper>
+            <AthleteForm />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Summary isfetching={isFetching} athletes={athletes} club={club} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper>
+            <h2>Transfer Athlete</h2>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default ManagedAthletes;
+export default ManageAthletes;
