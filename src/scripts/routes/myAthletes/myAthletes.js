7f763c0b20a5bbdc168d23e0aa82ebbc99c1b586
GDGunnars <gdg@trustpilot.com>
2019-04-09 00:05:07 +0200

    add athlete form & new grid element

--- src/scripts/routes/myAthletes/myAthletes.js
+++ src/scripts/routes/myAthletes/myAthletes.js
@@ -2,16 +2,24 @@ import React from "react";
 import Summary from "./summary";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import { AthleteForm } from "../../components";
 
 const MyAthletes = ({ isFetching, athletes, club }) => {
   return (
     <div>
       <Grid container spacing={16} direction="row-reverse">
         <Grid item xs={12} md={4}>
-          <Paper>xs </Paper>
+          <Paper>
+            <AthleteForm providedClub="HFH" />
+          </Paper>
         </Grid>
         <Grid item xs={12} md={8}>
           <Summary isfetching={isFetching} athletes={athletes} club={club} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper>
+            <h2>Request transfer</h2>
+          </Paper>
         </Grid>
       </Grid>
     </div>
