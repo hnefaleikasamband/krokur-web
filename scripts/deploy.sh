e9eba8d24fd793720e546095cf4e8c4619140fcc
GDGunnars <4069821+gdgunnars@users.noreply.github.com>
2021-03-28 16:06:05 +0000

    build: update travis & build scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,13 @@
+
 #!/bin/bash
-STAGE=$1
+BRANCH=$1
 STAGING="staging"
 PRODUCTION="prod"
+if [ "$BRANCH" != "$STAGING" ] && [ "$BRANCH" != "$PRODUCTION" ]; then
+  echo "you must pass in either \"staging\" or \"prod\""
+  exit 1
+fi
 
-case "$STAGE" in
-  "$STAGING")
-    echo -e "\u001b[32;7m Environment set to staging \u001b[0m"
-    APP_NAME=$APP_NAME_STAGING
-    REACT_APP_API_GATEWAY=$API_STAGING
-    ;;
-  "$PRODUCTION")
-    echo -e "\u001b[32;7m Environment set to production \u001b[0m"
-    APP_NAME=$APP_NAME_PRODUCTION
-    REACT_APP_API_GATEWAY=$API_PROD
-    ;;
-  *)
-    echo -e $"\u001b[31;1m Usage: $0 {staging|prod}\u001b[0m"
-    exit 1
-esac
-
-export REACT_APP_API_GATEWAY="$REACT_APP_API_GATEWAY"
-
-npm i --global caprover
-rm -r ./build
-npm run build --production
-tar -cvf ./deploy.tar --exclude="*.map" ./captain-definition ./build/*
-caprover deploy -t ./deploy.tar -h $CAP_HOST -a $APP_NAME -p $CAP_PASS > /dev/null 2>&1
+git config user.email "$GH_EMAIL"
+git config user.name "$GH_USER"
+git push https://$GH_USER:$GH_TOKEN@$GH_REPO HEAD:$BRANCH
